85cc02316ebab461d21df54cb8b720e32893abfc
mxhlde <milen.plevneliev@gmail.com>
2016-05-21 19:07:34 +0300

    Adding my ultra fast fibonacci algorithm in JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("My first git commit");
+var argv = require('yargs')
+    .usage('Usage: $0 --n [num]')
+    .demand(['n'])
+    .argv;
+
+var gr = 1.618033988749895;
+
+function fibonacci(n){
+  if (n == 0)
+    return 0;
+  if (n == 1)
+    return 1;
+    else {
+      return  Math.round(gr * fibonacci(n - 1));
+    }
+}
+
+console.log(fibonacci(argv.n));
